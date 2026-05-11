600945a88f13b88d51b33661ce845d5a8fbcd417
michellet4167 <2057636@learn.vsb.bc.ca>
2026-05-11 20:36:43 +0000

    hh

--- myApp.js
+++ myApp.js
@@ -1,10 +1,18 @@
+require('dotenv').config()
 let express = require('express');
 let app = express();
 
 console.log("Hello World");
 
-app.get("/", function(req, res) {
-    res.send("Hello Express");
+app.use("/public", express.static(__dirname + "/public"));
+    //res.sendFile(__dirname + "/views/index.html");
+    //res.json({
+app.get("/json", function(req,res) {
+if (process.env.MESSAGE_STYLE === "uppercase") {
+  res.json({"message": "HELLO JSON"});
+} else {
+  res.json({"message": "Hello json"});
+}
 });
 
 
