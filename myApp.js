c31f42b3225bdc68b46f21094e3b6950d57d33dc
michellet4167 <2057636@learn.vsb.bc.ca>
2026-05-12 19:57:47 +0000

    hhhh

--- myApp.js
+++ myApp.js
@@ -20,6 +20,25 @@ app.get("/now", middleware, (req, res) => {
     time: req.time
   });
 });
+
+app.get("/:word/echo", (req, res) => {
+  const { word } = req.params;
+  res.json({
+    echo: word
+  });
+});
+
+app.get("/name", function(req, res) {
+  var firstName = req.query.first;
+  var lastName = req.query.last;
+  // OR you can destructure and rename the keys
+  var { first: firstName, last: lastName } = req.query;
+  // Use template literals to form a formatted string
+  res.json({
+    name: `${firstName} ${lastName}`
+  });
+});
+
     //res.sendFile(__dirname + "/views/index.html");
     //res.json({
 app.get("/json", function(req,res) {
